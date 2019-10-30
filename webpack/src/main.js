2511ea4285fdd4603cc913ec1bfa865011586a86
wss <wussdyx@163.com>
2019-10-30 09:30:12 +0800

    webpack

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -5,6 +5,14 @@
 // ES6 的代码太高级了，浏览器解析不了，这一行直接执行会报错，需要用 webpack 处理该文件
 import $ from 'jquery'
 
+import './css/index.css'
+
+import './css/index.less'
+
+import './css/index.scss'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
 $(function(){
     $('li:odd').css('backgroundColor','pink')
     $('li:even').css('backgroundColor',function(){
---
+++ webpack_study/src/main.js
@@ -0,0 +1,2 @@
+// 项目的 JS 入口文件
+console.log('okk');
