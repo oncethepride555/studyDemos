acc306fe5845275212e5f5603a66e6bf2aa307db
wss <wussdyx@163.com>
2019-11-16 11:45:05 +0800

    vuex 中 mutations 和 getters 属性

--- vuex-study/src/main.js
+++ vuex-study/src/main.js
@@ -8,15 +8,31 @@ import vuex from 'vuex'
 Vue.use(vuex)
 // 4.new Vuex.Store() 实例，得到一个数据仓库对象
 const store = new vuex.Store({
-    state:{
+    state: {
         // 用来存储数据
         // 在组件中，想要访问state中的数据，通过 this.$store.state.数据名称
-        count:0
+        count: 0
     },
-    mutations:{
-        increment(state){
+    mutations: {
+        // 注意：如果要操作 state 中的数据的值，只能通过 调用 mutation 中的方法来操作对应的数据
+        // 不推荐直接操作 state 中的数据，因为万一导致了数据紊乱，不能快速定位到错误的原因，因为每个组件都有可能操作数据
+        increment(state) {
             state.count++
+        },
+        // 组件如果要调用 mutation 中的方法，通过 this.$store.commit('方法名')
+        substract(state, obj) {
+            // 注意：mutation 的函数参数列表中，最多支持两个参数
+            // 参数1：state  参数2：通过 commit 提交过来的参数
+            state.count -= (obj.a + obj.b);
         }
+    },
+    getters: {
+        // getters 只负责对外提供数据，不负责 修改数据，如果要修改 state 中的数据，要去找 mutation
+        strfun: function (state) {
+            return '当前最新的count值为：' + state.count;
+        }
+        // getters 中的方法，和组件中的过滤器相似，都没有修改原数据，只是把原数据做了一层包装，提供给了调用者
+        // 另外，getters 和 computed 也相似，只要 state 中的数据发生了变化，并且 getters 正好引用了这个数据，就会立即触发 getters 的重新求值。
     }
 })
 
