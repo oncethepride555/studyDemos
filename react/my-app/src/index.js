fec40065240bb4794d6b1f8664053e319383dff1
吴烁烁 <wushuoshuo@172-15-69-25.lightspeed.austtx.sbcglobal.net>
2020-11-24 16:27:13 +0800

    向class组件添加局部的state

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -11,11 +11,28 @@ import ReactDOM from 'react-dom';
 } */
 
 // 将函数组件转换成class组件
-class Clock extends React.Component {  // 第一步 创建同名class，继承自React.Component
+/* class Clock extends React.Component {  // 第一步 创建同名class，继承自React.Component
     render() {  // 第二步 空第render函数
         return (  // 第三步  函数体移过来   第四步 props前加this
             <div>
                 <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+} */
+
+// 向class组件添加局部的state
+class Clock extends React.Component {
+    // 第二步 添加class构造函数，为this.state赋初值
+    constructor(props) { // 将props传递到父类的构造函数中
+        super(props); // 使用super关键字调用一个父类的构造函数
+        this.state = { date: new Date() };
+    }
+    render() {
+        return (
+            // 第一步 this.props 改为 this.state
+            <div>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         );
     }
