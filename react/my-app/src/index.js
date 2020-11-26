a4d0b4ca9f201b6635cc6bb64125366e667c4be7
吴烁烁 <wushuoshuo@172-15-69-25.lightspeed.austtx.sbcglobal.net>
2020-11-26 15:13:41 +0800

    无障碍-tabindex属性

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -547,7 +547,65 @@ function WelcomDialog() {
     )
 }
 
+// 无障碍
+// tabindex 属性
+function SimpleControl() {
+    return (
+        // 没有tabindex 属性的话, 这些 <span> 元素不会被键盘focus中
+        <div>
+            <div>
+                <span role="checkbox" aria-checked="true" tabindex="0">
+                    <img src="checked.gif" role="presentation" alt="" />
+                    Include decorative fruit basket
+                </span>
+            </div>
+            <div>
+                <span role="checkbox" aria-checked="true" tabindex="0">
+                    <img src="checked.gif" role="presentation" alt="" />
+                    Include singing telegram
+                </span>
+            </div>
+            <div>
+                <span role="checkbox" aria-checked="false" tabindex="0">
+                    <img src="unchecked.gif" role="presentation" alt="" />
+                    Require payment before delivery
+                </span>
+            </div>
+        </div>
+    )
+}
 
+function GroupingControls() {
+    return (
+        <div>
+            <ul id="mb1" tabindex="0">
+                <li id="mb1_menu1"> Font
+                    <ul id="fontMenu" title="Font" tabindex="-1">
+                        <li id="sans-serif" tabindex="-1">Sans-serif</li>
+                        <li id="serif" tabindex="-1">Serif</li>
+                        <li id="monospace" tabindex="-1">Monospace</li>
+                        <li id="fantasy" tabindex="-1">Fantasy</li>
+                    </ul>
+                </li>
+                <li id="mb1_menu2" tabindex="-1"> Style
+                    <ul id="styleMenu" title="Style" tabindex="-1">
+                        <li id="italic" tabindex="-1">Italics</li>
+                        <li id="bold" tabindex="-1">Bold</li>
+                        <li id="underline" tabindex="-1">Underlined</li>
+                    </ul>
+                </li>
+                <li id="mb1_menu3" tabindex="-1"> Justification
+                    <ul id="justificationMenu" title="Justication" tabindex="-1">
+                        <li id="left" tabindex="-1">Left</li>
+                        <li id="center" tabindex="-1">Centered</li>
+                        <li id="right" tabindex="-1">Right</li>
+                        <li id="justify" tabindex="-1">Justify</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    )
+}
 
 const messages = ['react', 're:react', 're:re:react'];
 
@@ -570,6 +628,8 @@ function App() {
             <Calculator />
             <Dialog />
             <WelcomDialog />
+            <SimpleControl />
+            <GroupingControls />
         </div>
     )
 }
